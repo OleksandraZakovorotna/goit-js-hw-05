0800bacbfaed35f81c913b3d4d36a8f3c4d20af9
OleksandraZakovorotna <x.boogieboo.x@gmail.com>
2025-12-17 10:39:25 +0200

    Initial commit

---
+++ js/task-4.js
@@ -0,0 +1,26 @@
+// function getCommonElements(array1, array2) {
+//   let massive = [];
+//     for (let i = 0; i <= array1.length; i++) {
+//             if (array2.includes(array1[i])) {
+//                 massive.push(array1[i])
+//             }
+//         }
+//     return massive;
+// }
+
+// // const a1 = [1, 2, 3];
+// // const a2 = [2, 1, 17, 19];
+
+// // let aa1 = a1.join(" ");
+// // let aa2 = a2.join(" ");
+
+// // console.log(aa1)
+// // console.log(aa2)
+
+// // const la = aa1.includes(aa2);
+// // console.log(la)
+
+
+
+// console.log(getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27])); // "Shipping to Australia will cost 170 credits"
+
